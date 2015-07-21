16596d5c9351ac385fd8d8511fe163136bfee87f
zhaoalan <alanzhao@macmini.local>
2015-07-21 22:38:03 +0800

    cfg

---
+++ dgfh/dgfh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dgfh
+//
+//  Created by zhaoalan on 15/7/21.
+//  Copyright (c) 2015年 zhaoalan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
